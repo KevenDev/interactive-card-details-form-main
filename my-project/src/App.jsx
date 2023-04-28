05f196cff5319354d1ee8473f7f1280e62d1bc32
Keven Teles <glevenkeven@gmail.com>
2023-04-28 00:06:32 -0300

    feat: news state from app

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -5,12 +5,14 @@ import React, { useState} from 'react';
 function App() {
   const [firstName,setFirstName] = useState('XXXXXXXXX')
   const [card, setCard] = useState('0000 0000 0000 0000')
-
+  const [cvc, setCvc] = useState('000')
+  const [month, setMonth] = useState('00')
+  const [year, setYear] = useState('00')
   
   return (
     <>
-      <Card name={firstName} card={card}/>
-      <Form setName={setFirstName} setCard={setCard}/>
+      <Card name={firstName} card={card} cvc={cvc} month={month} year={year} />
+      <Form setName={setFirstName} setCard={setCard} setCvc={setCvc} setMonth={setMonth} setYear={setYear} />
     </>
   )
 }
